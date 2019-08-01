ec6eb1584f54c65bcb524e3d2e8558a39c498bc7
Yasmine <y.hartung@gmail.com>
2019-07-31 23:23:49 -0400

    working extension. final extension work

--- tabox_extension/content.js
+++ tabox_extension/content.js
@@ -1,30 +1,30 @@
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+// chrome.runtime.onMessage.addListener(
+//     function(request, sender, sendResponse) {
      
-         if(request.saveSession){
+//          if(request.saveSession){
 
-          localStorage.setItem("chromeSaveSession", request.saveSession)
-         }
-        if(request.restoreSession){
+//           localStorage.setItem("chromeSaveSession", request.saveSession)
+//          }
+//         if(request.restoreSession){
      
 
-          let session = localStorage.getItem("chromeRestoreSession")
+//           let session = localStorage.getItem("chromeRestoreSession")
           
-          if(session){
+//           if(session){
           
-            sendResponse({sessionToRestore: session})
-            localStorage.clear()
-          }else{
-            sendResponse({sessionToRestore: 'just fill'})
-          }
+//             sendResponse({sessionToRestore: session})
+//             localStorage.clear()
+//           }else{
+//             sendResponse({sessionToRestore: 'just fill'})
+//           }
           
-        }
-    });
-
+//         }
+//     });
+  console.log("from content")
     chrome.runtime.onMessage.addListener(
       function(request, sender, sendResponse) {
-       
+            console.log(request)
            if(request.saveSession){
   
             localStorage.setItem("chromeSaveSession", request.saveSession)
